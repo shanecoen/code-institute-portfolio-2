4f1f6c823d3688c1a318c5dc99d77150416d1b2d
Shaco <shanecoen@hotmail.com>
2022-03-29 12:30:50 +0000

    Declare js variables to represent elements

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Declare Variables To Represent Elements In Document
+let questionText = document.getElementById('question-title');
+let userScore = document.getElementById('total-score');
+let AnswerOne = document.getElementById('answer1');
+let AnswerTwo = document.getElementById('answer2');
+let AnswerThree = document.getElementById('answer3');
+let AnswerFour = document.getElementById('answer4');
+let restartBtn = document.getElementById('button-restart');
+let submitBtn = document.getElementById('button-submit');
