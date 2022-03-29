894a1c67448d5bdce70dcacbd08e5afb4f3ebd77
Shaco <shanecoen@hotmail.com>
2022-03-29 15:41:00 +0000

    Start iterate questions function script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // Declare Variables To Represent Elements In Document
 let questionText = document.getElementById('question-title');
 let userScore = document.getElementById('total-score');
-let AnswerOne = document.getElementById('answer1');
-let AnswerTwo = document.getElementById('answer2');
-let AnswerThree = document.getElementById('answer3');
-let AnswerFour = document.getElementById('answer4');
+let answerOne = document.getElementById('answer1');
+let answerTwo = document.getElementById('answer2');
+let answerThree = document.getElementById('answer3');
+let answerFour = document.getElementById('answer4');
 let restartBtn = document.getElementById('button-restart');
 let submitBtn = document.getElementById('button-submit');
 
@@ -12,45 +12,78 @@ let submitBtn = document.getElementById('button-submit');
 let startingQuestion = 0;
 let score = 0;
 
+
+
 // Quiz Questions
-let questions = [
-  { question: "Question 1 - The answer is option 1?",
+const questions = [{
+    id: 0,
+    question: "What is the capital of Ireland?",
     answers: [
-        {option: "Answer 1", answer: true},
-        {option: "Answer 2", answer: false},
-        {option: "Answer 3", answer: false},
-        {option: "Answer 4", answer: false}
+        {option: "Dublin", answer: true},
+        {option: "London", answer: false},
+        {option: "Paris", answer: false},
+        {option: "Barcelona", answer: false}
     ]},
-    { question: "Question 2 - The answer is option 2?",
+    { 
+    id: 1,
+    question: "What is the capital of Italy?",
     answers: [
-        {option: "Answer 1", answer: false},
-        {option: "Answer 2", answer: true},
-        {option: "Answer 3", answer: false},
-        {option: "Answer 4", answer: false}
+        {option: "Dublin", answer: false},
+        {option: "Rome", answer: true},
+        {option: "Turin", answer: false},
+        {option: "Madrid", answer: false}
     ]},
-    { question: "Question 3 - The answer is option 3?",
+    { 
+    id: 2,
+    question: "What is the capital of Spain?",
     answers: [
-        {option: "Answer 1", answer: false},
-        {option: "Answer 2", answer: false},
-        {option: "Answer 3", answer: true},
-        {option: "Answer 4", answer: false}
+        {option: "Dublin", answer: false},
+        {option: "Rome", answer: false},
+        {option: "Madrid", answer: true},
+        {option: "Paris", answer: false}
     ]},
-    { question: "Question 4 - The answer is option 4?",
+    { 
+    id: 3,  
+    question: "What is the capital of France?",
     answers: [
-        {option: "Answer 1", answer: false},
-        {option: "Answer 2", answer: false},
-        {option: "Answer 3", answer: false},
-        {option: "Answer 4", answer: true}
+        {option: "London", answer: false},
+        {option: "Madrid", answer: false},
+        {option: "Dublin", answer: false},
+        {option: "Paris", answer: true}
     ]},
-    { question: "Question 5 - The answer is option 1?",
+    { 
+    id: 4,   
+    question: "What is the capital of Germany?",
     answers: [
-        {option: "Answer 1", answer: true},
-        {option: "Answer 2", answer: false},
-        {option: "Answer 3", answer: false},
-        {option: "Answer 4", answer: false}
+        {option: "Berlin", answer: true},
+        {option: "Dublin", answer: false},
+        {option: "London", answer: false},
+        {option: "Paris", answer: false}
     ]}
 ]
 
-// Onlick events for Restart and Submit buttons 
-restartBtn.addEventListener('click', restart);
-submitBtn.addEventListener('click', submit);
+// Set Start
+let start = true;
+
+// Iterate
+function iterateQuestions(id) {
+    // Setting The Question
+    let questionText = document.getElementById('question-title');
+    
+    question-title.innerText = questions[id].question;
+
+    // Getting The Options
+    let answerOne = document.getElementById('answer1');
+    let answerTwo = document.getElementById('answer2');
+    let answerThree = document.getElementById('answer3');
+    let answerFour = document.getElementById('answer4');
+
+    // Providing Option text
+    answerOne.InnerText = questions[id].answers[0].text;
+    answerTwo.InnerText = questions[id].answers[1].text;
+    answerThree.InnerText = questions[id].answers[2].text;
+    answerFour.InnerText = questions[id].answers[3].text;
+
+
+}
+
